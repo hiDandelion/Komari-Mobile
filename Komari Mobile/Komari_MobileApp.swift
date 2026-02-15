10b5bf03b723a883fec69fbf80d8cd8ea9694feb
Junhui Lou <loujunhuimain@icloud.com>
2026-02-15 21:31:21 +0800

    Initial Commit

---
+++ Komari Mobile/Komari_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Komari_MobileApp.swift
+//  Komari Mobile
+//
+//  Created by Junhui Lou on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Komari_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
